c78b4d87dd341dc52105d0347bc741088f1fb190
esther <estherwa@hotmail.es>
2022-09-29 10:32:01 +0300

    dior web improvements fixed

--- data/products.js
+++ data/products.js
@@ -2,6 +2,6 @@ export const products = [
   { id: 1, name: 'Bag blue', img: 'dior.jpg', type: '0101 123456', price: '3.000$', details: 'Adorned with the CHRISTIAN DIOR PARIS signature on the front, the medium tote exemplifies the Houses signature savoir-faire and may be carried by hand or worn over the shoulder.' },
   { id: 2, name: 'Bag flower', img: 'dior2.jpg', type: '0101 123456', price: '3.500$', details: 'Adorned with the CHRISTIAN DIOR PARIS signature on the front, the medium tote exemplifies the Houses signature savoir-faire and may be carried by hand or worn over the shoulder. '},
   { id: 3, name: 'Bag desing', img: 'dior3.jpg',type: '0101 123456', price: '3.000$', details: 'Adorned with the CHRISTIAN DIOR PARIS signature on the front, the medium tote exemplifies the Houses signature savoir-faire and may be carried by hand or worn over the shoulder.' },
-  { id: 4, name: 'Bag style', img: 'dior4.jpg', type: '0101 123456', price: '3.006$', details: '  Introduced by Maria Grazia Chiuri, Creative Director of Christian Dior, the Dior Book Tote has become a staple of the Dior aesthetic. Designed to hold all the daily essentials.' },
+  { id: 4, name: 'Bag style', img: 'dior4.jpg', type: '0101 123456', price: '3.006$', details: '  Bag style introduced by Maria Grazia Chiuri, Creative Director of Christian Dior, the Dior Book Tote has become a staple of the Dior aesthetic. Designed to hold all the daily essentials.' },
   
 ]
