aeb2b1e379250e14d1bf201762e1046b4e8ac98b
esther <estherwa@hotmail.es>
2022-10-09 10:13:04 +0300

    fixed issues

--- utils/event.js
+++ utils/event.js
@@ -1,22 +1,26 @@
-const eventChannel = this.getOpenerEventChannel();
+
+
 function on (name , funct) {
+  const eventChannel = this.getOpenerEventChannel();
   eventChannel.on(name, funct)
 }
-function once({name, funct}) {
+function once({name,eventChannel, funct}) {
+  
   eventChannel.once(name, funct)
 }
 function emit({res,name}) {
+
+  
   if (res){
     res.eventChannel.emit(name);
   }
   else{
+    const eventChannel = this.getOpenerEventChannel();
     eventChannel.emit(name);
      }
 }
-function event({success,event}){
-    success: eventChannel.once('sendData'),
-    event
-}
 module.exports = {
-  event:  event
+  on:on,
+  once:once, 
+  emit:emit
 };
